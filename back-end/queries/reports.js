a012be6a55feb06d58115e8eda6f0888099a9d72
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-05-12 17:15:16 -0400

    the show and create routes created

--- back-end/queries/reports.js
+++ back-end/queries/reports.js
@@ -10,7 +10,34 @@ const getAllReports = async () => {
     }
 };
 
+// SINGLE REPORT
+const getReport = async (id) => {
+    try {
+        const oneReport = await db.one("SELECT * FROM reports WHERE id=$1", id);
+        return oneReport;
+    } catch (error) {
+        return error;
+    }
+};
+
+// CREATE A REPORT 
+const createReport = async (report) => {
+    const {location, transaction_date, deposit, name} = report;
+    try {
+        const newReport = await db.one(
+            "INSERT INTO reports (location, transaction_date, deposit, name) VALUES ($1, $2, $3, $4) RETURNING *",
+            [location, transaction_date, deposit, name]
+        );
+        return newReport;
+    } catch (error) {
+        return error;
+    }
+}
+
 
 module.exports = {
-    getAllReports
+    getAllReports,
+    createReport,
+    getReport,
+
 };

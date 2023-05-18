9ac3b89eeae3118b4827ccb5f500f32d14f8a715
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-05-17 22:52:26 -0400

    all packages installed components to render data created and material ui used for the tables

---
+++ front-end/src/components/navBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'; 
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className='navBar'>
+      <h1>
+        <Link to="/reports-gen">Report Generator</Link>
+      </h1>
+      <h1>
+        <Link to="/reports-gen">Bar Chart</Link>
+      </h1>
+      <h1>
+        <Link to="/reports-gen">Line Chart</Link>
+      </h1>
+      <h1>
+        <Link to="/reports-gen">Pie Chart</Link>
+      </h1>
+    </nav>
+  )
+}
+
+export default NavBar;
---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Reports from '../components/reports/Reports';
+
+const Home = () => {
+  return (
+    <div className='home'>
+        Home
+        <Reports />
+    </div>
+  )
+}
+
+export default Home
